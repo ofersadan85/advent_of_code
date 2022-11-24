eb6d497f896542ec4a56d5fb4b6b052dd46d5e22
Ofer Sadan <ofersadan85@gmail.com>
2022-11-24 14:36:08 +0200

    aoc_2021_5 done

--- src/aoc_2021_5.rs
+++ src/aoc_2021_5.rs
@@ -1,8 +1,7 @@
+use itertools::Itertools;
 use std::collections::HashMap;
 
-use itertools::Itertools;
-
-#[derive(Debug, Hash, PartialEq, Eq)]
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 struct Point {
     x: usize,
     y: usize,
@@ -17,30 +16,43 @@ struct Line {
 impl Line {
     fn get_points(&self) -> Vec<Point> {
         let mut result = vec![];
-        if self.start.x == self.end.x {
-            let v = vec![self.start.y, self.end.y];
-            let (min, max) = v.iter().minmax().into_option().unwrap();
-            let points = (min.to_owned()..=max.to_owned()).map(|i| Point {
-                x: self.start.x,
-                y: i,
-            });
-            result.extend(points);
+        let mut p = self.start.clone();
+        let x_inc = if self.start.x > self.end.x {
+            -1
+        } else if self.start.x < self.end.x {
+            1
+        } else {
+            0
+        };
+        let y_inc = if self.start.y > self.end.y {
+            -1
+        } else if self.start.y < self.end.y {
+            1
+        } else {
+            0
+        };
+        while (p.x, p.y) != (self.end.x, self.end.y) {
+            result.push(p);
+            p = Point {
+                x: (p.x as isize + x_inc) as usize,
+                y: (p.y as isize + y_inc) as usize,
+            }
         }
-        if self.start.y == self.end.y {
-            let v = vec![self.start.x, self.end.x];
-            let (min, max) = v.iter().minmax().into_option().unwrap();
-            let points = (min.to_owned()..=max.to_owned()).map(|i| Point {
-                x: i,
-                y: self.start.x,
-            });
-            result.extend(points);
-        }
+        result.push(self.end);
         result
+    }
+
+    fn is_diagonal(&self) -> bool {
+        self.start.x != self.end.x && self.start.y != self.end.y
     }
 }
 
-fn point_count(data: Vec<Line>) -> HashMap<Point, usize> {
-    let points: Vec<Point> = data.iter().flat_map(|line| line.get_points()).collect();
+fn point_count(data: Vec<Line>, diagonals: bool) -> HashMap<Point, usize> {
+    let points: Vec<Point> = data
+        .iter()
+        .filter(|line| diagonals || !line.is_diagonal())
+        .flat_map(|line| line.get_points())
+        .collect();
     let mut counter = HashMap::new();
     for p in points {
         counter.entry(p).and_modify(|v| *v += 1).or_insert(1);
@@ -50,8 +62,6 @@ fn point_count(data: Vec<Line>) -> HashMap<Point, usize> {
 
 #[cfg(test)]
 mod tests {
-    use itertools::Itertools;
-
     use super::*;
     use crate::common::*;
     const PATH: &str = "inputs/aoc_2021_5.txt";
@@ -66,7 +76,7 @@ mod tests {
     3,4 -> 1,4
     0,0 -> 8,8
     5,5 -> 8,2";
-    const EXAMPLE_OUTPUT: &str = "
+    const EXAMPLE_OUTPUT_1: &str = "
     .......1..
     ..1....1..
     ..1....1..
@@ -76,6 +86,17 @@ mod tests {
     ..........
     ..........
     ..........
+    222111....";
+    const EXAMPLE_OUTPUT_2: &str = "
+    1.1....11.
+    .111...2..
+    ..2.1.111.
+    ...1.2.2..
+    .112313211
+    ...1.2....
+    ..1...1...
+    .1.....1..
+    1.......1.
     222111....";
 
     fn setup_data(data: Vec<String>) -> Vec<Line> {
@@ -96,46 +117,48 @@ mod tests {
     }
 
     fn setup_output(points: HashMap<Point, usize>, w: usize, h: usize) -> String {
-        (0..h)
-            .map(|y| {
-                (0..w)
-                    .map(|x| points.get(&Point { x, y }).unwrap_or(&0).to_owned())
-                    .join("")
-            })
-            .rev()
-            .join("\n")
-            .replace('0', ".")
+        let mut board: Vec<Vec<String>> = (0..h)
+            .map(|_| (0..w).map(|_| ".".to_string()).collect())
+            .collect();
+        for (p, num) in points.iter() {
+            board[p.y][p.x] = num.to_string();
+        }
+        board.iter().map(|line| line.join("")).join("\n")
     }
 
     #[test]
     fn example_1() {
         let lines = split_lines(EXAMPLE);
         let data = setup_data(lines.clone());
-        let counter = point_count(data);
-        println!("{:?}", counter);
-        println!("{:?}", counter.values().sum::<usize>());
-        let result = setup_output(counter, lines[0].len(), lines.len());
-        assert_eq!(result, split_lines(EXAMPLE_OUTPUT).join("\n"));
+        let counter = point_count(data, false);
+        let result = counter.values().filter(|v| **v >= 2).count();
+        assert_eq!(result, 5);
     }
 
-    // #[test]
-    // fn example_2() {
-    //     let data = setup_data(split_lines(EXAMPLE));
-    //     let result = do_something(data);
-    //     assert_eq!(result, EXAMPLE_OUTPUT);
-    // }
+    #[test]
+    fn example_2() {
+        let lines = split_lines(EXAMPLE);
+        let data = setup_data(lines.clone());
+        let counter = point_count(data, true);
+        let result = counter.values().filter(|v| **v >= 2).count();
+        assert_eq!(result, 12);
+    }
 
-    // #[test]
-    // fn task_1() {
-    //     let data = setup_data(get_data(PATH).unwrap());
-    //     let result = do_something(data);
-    //     assert_eq!(result, EXAMPLE_OUTPUT);
-    // }
+    #[test]
+    fn task_1() {
+        let lines = get_data(PATH).unwrap();
+        let data = setup_data(lines.clone());
+        let counter = point_count(data, false);
+        let result = counter.values().filter(|v| **v >= 2).count();
+        assert_eq!(result, 6267);
+    }
 
-    // #[test]
-    // fn task_2() {
-    //     let data = setup_data(get_data(PATH).unwrap());
-    //     let result = do_something(data);
-    //     assert_eq!(result, EXAMPLE_OUTPUT);
-    // }
+    #[test]
+    fn task_2() {
+        let lines = get_data(PATH).unwrap();
+        let data = setup_data(lines.clone());
+        let counter = point_count(data, true);
+        let result = counter.values().filter(|v| **v >= 2).count();
+        assert_eq!(result, 20196);
+    }
 }
