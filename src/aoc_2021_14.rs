15b0b1a714626fb9d1461e49398bec0a4924c417
Ofer Sadan <ofersadan85@gmail.com>
2022-11-28 13:42:53 +0200

    aoc_2021_14 done

--- src/aoc_2021_14.rs
+++ src/aoc_2021_14.rs
@@ -1,110 +1,142 @@
-// use std::collections::HashMap;
+use std::{collections::HashMap};
 
-// use itertools::iproduct;
+use itertools::{iproduct, Itertools};
 
-// type CharPair = (char, char);
-// type PairCounter = HashMap<CharPair, usize>;
-// type ChainRules = HashMap<CharPair, (CharPair, CharPair)>;
+type CharPair = (char, char);
+type PairCounter = HashMap<CharPair, i64>;
+type ChainRules = HashMap<CharPair, (CharPair, CharPair)>;
 
-// fn char_pair_counter() -> PairCounter {
-//     iproduct!(
-//         "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars(),
-//         "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars()
-//     )
-//     .map(|(a, b)| ((a, b), 0))
-//     .collect()
-// }
+fn char_pair_counter() -> PairCounter {
+    iproduct!(
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars(),
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars()
+    )
+    .map(|(a, b)| ((a, b), 0))
+    .collect()
+}
 
-// fn apply_rules(pairs: &mut PairCounter, rules: ChainRules) {
-//     let mut new_pairs = pairs.clone();
-//     for (rule, (pair1, pair2)) in rules {
-//         let value = pairs.get(&rule).unwrap().to_owned();
-//         *new_pairs.get_mut(&rule).unwrap() -= value;
-//         *new_pairs.get_mut(&pair1).unwrap() += value;
-//         *new_pairs.get_mut(&pair2).unwrap() += value;
-//     }
-//     pairs = &mut new_pairs;
-// }
+fn apply_rules(pairs: &mut PairCounter, rules: &ChainRules) {
+    let mut operations: HashMap<(char, char), i64> = HashMap::new();
+    for (rule, (pair1, pair2)) in rules {
+        let value = pairs.get(rule).unwrap().to_owned();
+        operations
+            .entry(*rule)
+            .and_modify(|v| *v -= value)
+            .or_insert(-value);
+        operations
+            .entry(*pair1)
+            .and_modify(|v| *v += value)
+            .or_insert(value);
+        operations
+            .entry(*pair2)
+            .and_modify(|v| *v += value)
+            .or_insert(value);
+    }
+    for (key, value) in operations {
+        *pairs.get_mut(&key).unwrap() += value;
+    }
+}
 
-// #[cfg(test)]
-// mod tests {
-//     use itertools::Itertools;
+fn count_chars(pairs: &PairCounter, edges: &CharPair) -> i64 {
+    let mut result: HashMap<char, i64> = HashMap::new();
+    for ((char1, char2), count) in pairs.iter().filter(|(_, &v)| v > 0) {
+        result.entry(*char1).and_modify(|v| *v += count).or_insert(*count);
+        result.entry(*char2).and_modify(|v| *v += count).or_insert(*count);
+    }
+    result.entry(edges.0).and_modify(|v| *v += 1);
+    result.entry(edges.1).and_modify(|v| *v += 1);
 
-//     use super::*;
-//     use crate::common::*;
-//     const PATH: &str = "inputs/aoc_2021_14.txt";
-//     const EXAMPLE: &str = "NNCB
+    let (min, max) = result.iter().minmax_by_key(|(_, &v)| v).into_option().unwrap();
+    (max.1 - min.1) / 2
+}
 
-//     CH -> B
-//     HH -> N
-//     CB -> H
-//     NH -> C
-//     HB -> C
-//     HC -> B
-//     HN -> C
-//     NN -> C
-//     BH -> H
-//     NC -> B
-//     NB -> B
-//     BN -> B
-//     BB -> N
-//     BC -> B
-//     CC -> N
-//     CN -> C";
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::common::*;
+    const PATH: &str = "inputs/aoc_2021_14.txt";
+    const EXAMPLE: &str = "NNCB
 
-//     fn setup_data(data: Vec<String>) -> (PairCounter, ChainRules, CharPair) {
-//         let mut counter = char_pair_counter();
-//         let v: Vec<char> = data[0].chars().collect();
-//         for window in v.windows(2) {
-//             *counter.get_mut(&(window[0], window[1])).unwrap() += 1;
-//         }
+    CH -> B
+    HH -> N
+    CB -> H
+    NH -> C
+    HB -> C
+    HC -> B
+    HN -> C
+    NN -> C
+    BH -> H
+    NC -> B
+    NB -> B
+    BN -> B
+    BB -> N
+    BC -> B
+    CC -> N
+    CN -> C";
 
-//         let arrow = " -> ";
-//         let rules = data
-//             .iter()
-//             .filter(|s| s.contains(arrow))
-//             .map(|s| {
-//                 let mut split = s.split(arrow);
-//                 let mut left = split.next().unwrap().chars();
-//                 let a = left.next().unwrap();
-//                 let b = left.next().unwrap();
-//                 let c = split.next().unwrap().chars().next().unwrap();
-//                 ((a, b), ((a, c), (c, b)))
-//             })
-//             .collect();
+    fn setup_data(data: Vec<String>) -> (PairCounter, ChainRules, CharPair) {
+        let mut counter = char_pair_counter();
+        let v: Vec<char> = data[0].chars().collect();
+        for window in v.windows(2) {
+            *counter.get_mut(&(window[0], window[1])).unwrap() += 1;
+        }
 
-//         let edges = (v.first().unwrap().to_owned(), v.last().unwrap().to_owned());
+        let arrow = " -> ";
+        let rules = data
+            .iter()
+            .filter(|s| s.contains(arrow))
+            .map(|s| {
+                let mut split = s.split(arrow);
+                let mut left = split.next().unwrap().chars();
+                let a = left.next().unwrap();
+                let b = left.next().unwrap();
+                let c = split.next().unwrap().chars().next().unwrap();
+                ((a, b), ((a, c), (c, b)))
+            })
+            .collect();
 
-//         (counter, rules, edges)
-//     }
+        let edges = (v.first().unwrap().to_owned(), v.last().unwrap().to_owned());
 
-//     #[test]
-//     fn example_1() {
-//         let (mut pairs, rules) = setup_data(split_lines(EXAMPLE));
-//         // apply_rules(&mut pairs, rules);
-//         let result = pairs.iter().filter(|(_, &v)| v > 0).collect_vec();
-//         println!("{:?}", result);
-//         assert_eq!(323, 123);
-//     }
+        (counter, rules, edges)
+    }
 
-//     // #[test]
-//     // fn example_2() {
-//     //     let data = setup_data(split_lines(EXAMPLE));
-//     //     let result: usize = calc_fuel_increasing(data);
-//     //     assert_eq!(result, 168);
-//     // }
+    #[test]
+    fn example_1() {
+        let mut data = setup_data(split_lines(EXAMPLE));
+        for _ in 0..10 {
+            apply_rules(&mut data.0, &data.1);
+        }
+        let result = count_chars(&data.0, &data.2);
+        assert_eq!(result, 1588);
+    }
 
-//     // #[test]
-//     // fn task_1() {
-//     //     let data = setup_data(get_data(PATH).unwrap());
-//     //     let result: usize = calc_fuel(data);
-//     //     assert_eq!(result, 356958);
-//     // }
+    #[test]
+    fn example_2() {
+        let mut data = setup_data(split_lines(EXAMPLE));
+        for _ in 0..40 {
+            apply_rules(&mut data.0, &data.1);
+        }
+        let result = count_chars(&data.0, &data.2);
+        assert_eq!(result, 2188189693529);
+    }
 
-//     // #[test]
-//     // fn task_2() {
-//     //     let data = setup_data(get_data(PATH).unwrap());
-//     //     let result: usize = calc_fuel_increasing(data);
-//     //     assert_eq!(result, 105461913);
-//     // }
-// }
+    #[test]
+    fn task_1() {
+        let mut data = setup_data(get_data(PATH).unwrap());
+        for _ in 0..10 {
+            apply_rules(&mut data.0, &data.1);
+        }
+        let result = count_chars(&data.0, &data.2);
+        assert_eq!(result, 2587);
+    }
+
+    #[test]
+    fn task_2() {
+        let mut data = setup_data(get_data(PATH).unwrap());
+        for _ in 0..40 {
+            apply_rules(&mut data.0, &data.1);
+        }
+        let result = count_chars(&data.0, &data.2);
+        assert_eq!(result, 3318837563123);
+    }
+}
