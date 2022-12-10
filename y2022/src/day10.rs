890a7bc977efbd444ad1c525841ef6637896f05b
Ofer Sadan <ofersadan85@gmail.com>
2022-12-10 14:58:37 +0200

    y2022 day10 done

---
+++ y2022/src/day10.rs
@@ -0,0 +1,92 @@
+use advent_of_code_common::file::split_lines_trim;
+
+const PATH: &str = "inputs/day10.txt";
+const EXAMPLE: &str = "inputs/day10_example.txt";
+const EXPECTED_EXAMPLE: &str = "##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....";
+const EXPECTED_PART_2: &str = "###...##..###..#..#.####.#..#.####...##.
+...#.#..#.#..#.#.#..#....#.#..#.......#.
+...#.#..#.#..#.##...###..##...###.....#.
+###..####.###..#.#..#....#.#..#.......#.
+.....#..#.#....#.#..#....#.#..#....#..#.
+.....#..#.#....#..#.#....#..#.####..##.."; // PAPKFKEJ
+
+fn input(example: bool) -> Vec<String> {
+    let path = if example { EXAMPLE } else { PATH };
+    split_lines_trim(&std::fs::read_to_string(path).unwrap())
+}
+
+#[allow(clippy::cast_sign_loss)] // TODO: Find a better alternative
+fn calc_sprite_position(data: &[String]) -> [usize; 240] {
+    let mut cycles: [i16; 240] = [0; 240]; // 240 refers to part_2 (40 * 6)
+    let mut count = 0;
+    cycles[0] = 1;
+    for (i, row) in data.iter().enumerate() {
+        if row.starts_with("add") {
+            count += 1;
+            cycles[i + count] = row
+                .split_ascii_whitespace()
+                .last()
+                .unwrap()
+                .parse()
+                .unwrap();
+        }
+    }
+    let mut result: [usize; 240] = [0; 240];
+    for i in 0..240 {
+        result[i] = cycles[..i].iter().sum::<i16>() as usize;
+    }
+    result
+}
+
+fn part_1(data: &[String]) -> usize {
+    let sprite_positions = calc_sprite_position(data);
+    sprite_positions[19..220]
+        .iter()
+        .step_by(40)
+        .enumerate()
+        .map(|(index, n)| (index * 40 + 20) * n)
+        .sum()
+}
+
+fn part_2(data: &[String]) -> String {
+    let mut result = String::new();
+    let sprite_positions = calc_sprite_position(data);
+    for (i, &p) in sprite_positions.iter().enumerate() {
+        let idx = i % 40;
+        if idx + 1 == p || idx == p || (idx > 0 && idx - 1 == p) {
+            result.push('#');
+        } else {
+            result.push('.');
+        }
+        if i % 40 == 39 && i < 239 {
+            result.push('\n');
+        }
+    }
+    result
+}
+
+#[test]
+fn example_1() {
+    assert_eq!(part_1(&input(true)), 13140);
+}
+
+#[test]
+fn task_1() {
+    assert_eq!(part_1(&input(false)), 14060);
+}
+
+#[test]
+fn example_2() {
+    assert_eq!(part_2(&input(true)), EXPECTED_EXAMPLE);
+}
+
+#[test]
+fn task_2() {
+    let output = part_2(&input(false));
+    assert_eq!(output, EXPECTED_PART_2, "\n{}\n", output);
+}
