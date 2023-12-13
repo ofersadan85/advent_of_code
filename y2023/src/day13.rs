bdf9141af600a6feee86ccafa9e84b16256cdc17
Ofer Sadan <ofersadan85@gmail.com>
2023-12-13 15:06:35 +0200

    day13 part 2

--- y2023/src/day13.rs
+++ y2023/src/day13.rs
@@ -1,5 +1,5 @@
 use anyhow::{anyhow, Result};
-use tracing::{info, instrument, warn};
+use tracing::instrument;
 
 pub const EXAMPLE1: &str = "#.##..##.
 ..#.##.#.
@@ -44,12 +44,9 @@ fn reflection_h(input: &str) -> Option<usize> {
     None
 }
 
-#[instrument(skip_all, ret, level = "info")]
+#[instrument(skip_all, level = "debug")]
 fn reflection_h_smudged(input: &str) -> Option<usize> {
     let previous_value = reflection_h(input);
-    for line in input.lines() {
-        info!("{}", line);
-    }
     let lines: Vec<_> = input.lines().collect();
     let mut smudge_found = false;
     for index in 0..lines.len() - 1 {
@@ -63,29 +60,19 @@ fn reflection_h_smudged(input: &str) -> Option<usize> {
                 .count();
             match (diff_count, smudge_found) {
                 (0, _) => {
-                    info!(
-                        "Equal rows {} {}-{} {}",
-                        smudge_found,
-                        inspected_index + 1,
-                        reflected_index + 1,
-                        lines[inspected_index]
-                    );
-                    if inspected_index == 0
-                        || reflected_index == lines.len() - 1
-                            && previous_value != Some(index + 1)
-                            && smudge_found
+                    if (inspected_index == 0 || reflected_index == lines.len() - 1)
+                        && previous_value != Some(index + 1)
+                        && smudge_found
                     {
                         return Some(index + 1);
                     }
                 }
                 (1, false) => {
                     // There's one difference, and it's the first one we've found
-                    info!("There's one smudge at index {}", index + 1);
                     smudge_found = true;
-                    if inspected_index == 0
-                        || reflected_index == lines.len() - 1
-                            && previous_value != Some(index + 1)
-                            && smudge_found
+                    if (inspected_index == 0 || reflected_index == lines.len() - 1)
+                        && previous_value != Some(index + 1)
+                        && smudge_found
                     {
                         return Some(index + 1);
                     }
@@ -132,20 +119,17 @@ fn reflection_v(input: &str, smudged: bool) -> Option<usize> {
     }
 }
 
-#[instrument(skip_all, ret, level = "info")]
+#[instrument(skip_all, level = "debug")]
 fn box_value(input: &str, smudged: bool) -> usize {
     let value = if smudged {
         reflection_h_smudged(input)
     } else {
         reflection_h(input)
     };
-    match value {
-        Some(index) => index * 100,
-        None => {
-            info!("No horizontal reflection found");
-            reflection_v(input, smudged).unwrap_or(0)
-        }
-    }
+    value.map_or_else(
+        || reflection_v(input, smudged).unwrap_or(0),
+        |index| index * 100,
+    )
 }
 
 #[instrument(skip(input), level = "debug")]
@@ -198,7 +182,6 @@ mod tests {
 
     #[test]
     fn part2_example() {
-        init_tracing();
         assert_eq!(mirrors(EXAMPLE1, true), 300);
         assert_eq!(mirrors(EXAMPLE1_ROTATED, true), 3);
         assert_eq!(mirrors(EXAMPLE2, true), 100);
@@ -206,7 +189,6 @@ mod tests {
 
     #[test]
     fn part2() {
-        assert_ne!(mirrors(include_str!("day13.txt"), true), 31110);
-        assert!(mirrors(include_str!("day13.txt"), true) > 36597);
+        assert_eq!(mirrors(include_str!("day13.txt"), true), 37876);
     }
 }
