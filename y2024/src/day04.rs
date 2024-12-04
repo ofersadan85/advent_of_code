cc6d57d37a342a43c3844017fa34cd6dc504df3a
Ofer Sadan <ofersadan85@gmail.com>
2024-12-04 12:44:10 +0200

    simplified as Grid<char>

--- y2024/src/day04.rs
+++ y2024/src/day04.rs
@@ -1,34 +1,12 @@
 use advent_of_code_common::grid::Grid;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-enum Letter {
-    X,
-    M,
-    A,
-    S,
-}
-
-impl TryFrom<char> for Letter {
-    type Error = ();
-
-    fn try_from(value: char) -> Result<Self, Self::Error> {
-        match value {
-            'X' => Ok(Self::X),
-            'M' => Ok(Self::M),
-            'A' => Ok(Self::A),
-            'S' => Ok(Self::S),
-            _ => Err(()),
-        }
-    }
-}
-
-fn find_sequences(grid: &Grid<Letter>) -> usize {
-    let search = [Letter::M, Letter::A, Letter::S]; // Doesn't include the starting X
+fn find_sequences(grid: &Grid<char>) -> usize {
+    let search: Vec<char> = "MAS".chars().collect(); // Doesn't include the starting X
     grid.cells
         .iter()
-        .filter(|c| c.state == Letter::X)
+        .filter(|c| c.state == 'X')
         .map(|cell| {
-            grid.sight_lines_all(cell.x, cell.y, &[Letter::S])
+            grid.sight_lines_all(cell.x, cell.y, &['S'])
                 .iter()
                 .filter(|seq| seq.as_slice() == search)
                 .count()
@@ -36,15 +14,15 @@ fn find_sequences(grid: &Grid<Letter>) -> usize {
         .sum()
 }
 
-fn find_diagonal_sequences(grid: &Grid<Letter>) -> usize {
+fn find_diagonal_sequences(grid: &Grid<char>) -> usize {
     grid.cells
         .iter()
-        .filter(|c| c.state == Letter::A)
+        .filter(|c| c.state == 'A')
         .map(|cell| match grid.neighbors_diagonal(cell.x, cell.y) {
-            [Some(Letter::M), Some(Letter::M), Some(Letter::S), Some(Letter::S)]
-            | [Some(Letter::M), Some(Letter::S), Some(Letter::S), Some(Letter::M)]
-            | [Some(Letter::S), Some(Letter::S), Some(Letter::M), Some(Letter::M)]
-            | [Some(Letter::S), Some(Letter::M), Some(Letter::M), Some(Letter::S)] => 1,
+            [Some('M'), Some('M'), Some('S'), Some('S')]
+            | [Some('M'), Some('S'), Some('S'), Some('M')]
+            | [Some('S'), Some('S'), Some('M'), Some('M')]
+            | [Some('S'), Some('M'), Some('M'), Some('S')] => 1,
             _ => 0,
         })
         .sum()
@@ -58,28 +36,28 @@ mod tests {
     #[test]
     fn test_example_1() {
         let input = read_to_string("../inputs/2024/day04_example.txt").unwrap();
-        let grid: Grid<Letter> = input.parse().unwrap();
+        let grid: Grid<char> = input.parse().unwrap();
         assert_eq!(find_sequences(&grid), 18);
     }
 
     #[test]
     fn test_part_1() {
         let input = read_to_string("../inputs/2024/day04.txt").unwrap();
-        let grid: Grid<Letter> = input.parse().unwrap();
+        let grid: Grid<char> = input.parse().unwrap();
         assert_eq!(find_sequences(&grid), 2517);
     }
 
     #[test]
     fn test_example_2() {
         let input = read_to_string("../inputs/2024/day04_example.txt").unwrap();
-        let grid: Grid<Letter> = input.parse().unwrap();
+        let grid: Grid<char> = input.parse().unwrap();
         assert_eq!(find_diagonal_sequences(&grid), 9);
     }
 
     #[test]
     fn test_part_2() {
         let input = read_to_string("../inputs/2024/day04.txt").unwrap();
-        let grid: Grid<Letter> = input.parse().unwrap();
+        let grid: Grid<char> = input.parse().unwrap();
         assert_eq!(find_diagonal_sequences(&grid), 1960);
     }
 }
