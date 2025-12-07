6ff45fc23bfd3a3dca5cb0a5dbc529fc313503a6
Ofer Sadan <ofersadan85@gmail.com>
2025-12-07 12:13:19 +0200

    better efficiency for y2025 day07

--- y2025/src/day07.rs
+++ y2025/src/day07.rs
@@ -1,11 +1,20 @@
 use advent_of_code_common::Solver;
-use advent_of_code_common::grid::{Direction, Grid, Point};
+use advent_of_code_common::grid::{Coords, Direction, Grid, Point};
 
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 enum CellData {
     Empty,
     Splitter,
     Beam(usize),
+}
+
+impl CellData {
+    const fn get_beam_count(&self) -> usize {
+        match self {
+            Self::Empty | Self::Splitter => 0,
+            Self::Beam(n) => *n,
+        }
+    }
 }
 
 impl TryFrom<char> for CellData {
@@ -21,54 +30,47 @@ impl TryFrom<char> for CellData {
     }
 }
 
-fn propagate_beams(input: &str) -> (usize, usize) {
-    let mut grid: Grid<CellData> = input.parse().expect("valid grid");
+fn grid_step(grid: &mut Grid<CellData>, point: Point) -> usize {
+    let beam = grid
+        .get(&point)
+        .map_or(0, |cell| cell.data.get_beam_count());
+    if beam == 0 {
+        return 0;
+    }
     let mut split_count = 0;
-    let mut is_last_step = false;
-    while !is_last_step {
-        let current_beams: Vec<(Point, usize)> = grid
-            .iter()
-            .filter_map(|(point, cell)| match cell.data {
-                CellData::Empty | CellData::Splitter => None,
-                CellData::Beam(n) => Some((*point, n)),
-            })
-            .collect();
-        for (beam, n) in current_beams {
-            if beam.y >= grid.height() - 2 {
-                is_last_step = true;
-            }
-            if let Some(cell) = grid.get_mut(&beam) {
-                cell.data = CellData::Empty;
-            }
-            let below = beam + Direction::South;
-            if let Some(cell) = grid.get_mut(&below) {
-                match cell.data {
-                    CellData::Empty => cell.data = CellData::Beam(n),
-                    CellData::Splitter => {
-                        split_count += 1;
-                        for dir in [Direction::SouthWest, Direction::SouthEast] {
-                            if let Some(cell) = grid.get_mut(&(beam + dir)) {
-                                match cell.data {
-                                    CellData::Empty => cell.data = CellData::Beam(n),
-                                    CellData::Beam(count) => cell.data = CellData::Beam(count + n),
-                                    CellData::Splitter => unreachable!("touching splitters"),
-                                }
-                            }
+    grid.get_mut(&point).expect("cell exists").data = CellData::Empty;
+    let below = point + Direction::South;
+    if let Some(cell) = grid.get_mut(&below) {
+        match cell.data {
+            CellData::Empty => cell.data = CellData::Beam(beam),
+            CellData::Splitter => {
+                split_count += 1;
+                for dir in [Direction::SouthWest, Direction::SouthEast] {
+                    let next_point = point + dir;
+                    if let Some(cell) = grid.get_mut(&next_point) {
+                        cell.data = match cell.data {
+                            CellData::Empty => CellData::Beam(beam),
+                            CellData::Beam(count) => CellData::Beam(count + beam),
+                            CellData::Splitter => unreachable!("touching splitters"),
                         }
                     }
-                    CellData::Beam(below_n) => cell.data = CellData::Beam(below_n + n),
                 }
             }
+            CellData::Beam(value) => cell.data = CellData::Beam(value + beam),
         }
     }
-    let timelines = grid
-        .iter()
-        .filter_map(|(_, cell)| match cell.data {
-            CellData::Beam(n) => Some(n),
-            _ => None,
-        })
-        .sum();
-    (split_count, timelines)
+    split_count
+}
+
+fn propagate_beams(input: &str) -> (Grid<CellData>, usize) {
+    let mut grid: Grid<CellData> = input.parse().expect("valid grid");
+    let mut split_count = 0;
+    for y in grid.y_range.start..grid.y_range.end - 1 {
+        for x in grid.x_range.clone() {
+            split_count += grid_step(&mut grid, (x, y).as_point());
+        }
+    }
+    (grid, split_count)
 }
 
 struct Part1;
@@ -76,7 +78,7 @@ impl Solver<'_> for Part1 {
     type Output = usize;
 
     fn solve(&self, input: &str) -> Self::Output {
-        propagate_beams(input).0
+        propagate_beams(input).1
     }
 
     fn file_path(&self) -> std::path::PathBuf {
@@ -89,7 +91,12 @@ impl Solver<'_> for Part2 {
     type Output = usize;
 
     fn solve(&self, input: &str) -> Self::Output {
-        propagate_beams(input).1
+        let grid = propagate_beams(input).0;
+        grid.x_range
+            .clone()
+            .filter_map(|x| grid.get(&(x, grid.y_range.end - 1)))
+            .map(|cell| cell.data.get_beam_count())
+            .sum()
     }
 
     fn file_path(&self) -> std::path::PathBuf {
