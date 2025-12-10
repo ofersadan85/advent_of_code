23876039d45a55609dd2692b66b9914287b79ae9
Ofer Sadan <ofersadan85@gmail.com>
2025-12-10 08:09:10 +0200

    prep for days 10-12

---
+++ y2025/src/day10.rs
@@ -0,0 +1,45 @@
+use advent_of_code_common::Solver;
+
+struct Part1;
+impl Solver<'_> for Part1 {
+    type Output = usize;
+
+    fn solve(&self, _input: &str) -> Self::Output {
+        todo!()
+    }
+
+    fn file_path(&self) -> std::path::PathBuf {
+        crate::default_input_path!()
+    }
+}
+
+struct Part2;
+impl Solver<'_> for Part2 {
+    type Output = usize;
+
+    fn solve(&self, _input: &str) -> Self::Output {
+        todo!()
+    }
+
+    fn file_path(&self) -> std::path::PathBuf {
+        crate::default_input_path!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use advent_of_code_common::expect_solution;
+
+    #[test]
+    fn part_1() {
+        expect_solution!(Part1, 0, 50);
+        // expect_solution!(Part1, 1, 4741451444);
+    }
+
+    // #[test]
+    // fn part_2() {
+    //     expect_solution!(Part2, 0, 24);
+    //     expect_solution!(Part2, 1, 1562459680);
+    // }
+}
--- y2025/src/lib.rs
+++ y2025/src/lib.rs
@@ -1,4 +1,15 @@
-advent_of_code_macros::all_the_days!(9);
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
+mod day10;
+mod day11;
+mod day12;
 
 #[macro_export]
 macro_rules! default_input_path {
